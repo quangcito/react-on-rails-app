895cb152d6226c893bbe89dc8e00ed497cc981c0
Quang Duong Nguyen <qnguyen2@macalester.edu>
2023-11-27 00:25:08 -0600

    Added links to titles

--- client/src/features/posts/PostsList.jsx
+++ client/src/features/posts/PostsList.jsx
@@ -1,6 +1,7 @@
 // API_URL comes from .env.development file
-import React, { useState, useEffect} from 'react';
-import { API_URL } from '../../constants';
+import React, { useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { API_URL } from '../../constants'
 
 function PostsList() {
   const [posts, setPosts] = useState([])
@@ -32,7 +33,11 @@ function PostsList() {
     <div>
     {posts.map((post) => (
       <div key={post.id} className='post-container'>
-        <h2>{post.title}</h2>
+        <h2>
+          <Link to={`/posts/${post.id}`} className='post-title'>
+            {post.title}
+          </Link>
+        </h2>
         <p>{post.body}</p>
       </div>
     ))}
